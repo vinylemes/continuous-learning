6a20d1ba70fa699d66c4c532b3aac573b5bca7e0
Vinícius Lemes <vlemesalves@gmail.com>
2022-03-29 22:45:57 -0300

    JS para backend Alura

---
+++ javascript/basics/alura-js-backend/declarandoVariaveis.js
@@ -0,0 +1,13 @@
+const firstName = "Vinícius";
+var middleName = "Lemes";
+let lastName = "Alves";
+console.log(firstName + middleName + lastName);
+
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+const firstNumber = 123;
+const secondNumber = "123";
+console.log(firstNumber + secondNumber);
+console.log(firstNumber + Number(secondNumber));
